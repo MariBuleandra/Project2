33457178033365f892aad74379c528c4b5a5acaa
mirelpop <mirel.pop@modaoperandi.com>
2019-10-22 13:18:23 +0300

    Added new message

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,5 +3,6 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Branch1");    }
+        System.out.println("Error");    }
+        System.out.println("Added the second branch");    }
 }
