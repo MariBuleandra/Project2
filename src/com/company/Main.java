609d611b81e50a7837183e222d3c6583d0226446
mirelpop <mirel.pop@modaoperandi.com>
2019-10-22 13:29:36 +0300

    Removed "Error" message as requested.

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,7 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Error");    }
+
+    }
         System.out.println("Added the second branch");    }
 }
